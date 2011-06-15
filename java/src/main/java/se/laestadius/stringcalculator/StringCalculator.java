234f288773c8e0b06b412faf23beb28865581f7f
Anders Laestadius <anders@laestadius.se>
2011-06-15 22:07:54 +0200

    new version

---
+++ java/src/main/java/se/laestadius/stringcalculator/StringCalculator.java
@@ -0,0 +1,5 @@
+package se.laestadius.stringcalculator;
+
+public class StringCalculator {
+
+}
---
+++ java/src/test/java/se/laestadius/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package se.laestadius.stringcalculator;
+
+import org.junit.Test;
+ 
+public class StringCalculatorTest {
+
+	private final StringCalculator calculator = new StringCalculator();
+	
+	@Test
+	public void emptyStringShouldAddTo_0() {
+		
+	}
+}
